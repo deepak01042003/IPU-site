59471034599cf56611d7a837853560d903c6cd20
deepak01042003 <deepakgarg01042003@gmail.com>
2025-03-31 19:12:47 +0530

    Created IPU SITE

---
+++ frotend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 4000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '.')));
+
+app.listen(4000, () => {
+    console.log(`Frontend server listening at http://localhost:4000`);
+});
